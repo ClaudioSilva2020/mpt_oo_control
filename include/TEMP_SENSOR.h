a1751bfcb7a8b5a398613163a75c4326f4dcc0b3
claudiosilva <claudioccdsilva@gmail.com>
2020-09-04 07:59:46 -0300

    atualizo versão

--- include/TEMP_SENSOR.h
+++ include/TEMP_SENSOR.h
@@ -16,24 +16,3 @@ unsigned int TEMP_read();
 
 #endif
 
-unsigned int TEMP_read()
-{
-    float of_set = 300;
-    double temp_internal;
-    // Seta modo de leitura de temperatura interna
-    ADMUX |= (1<<MUX0)|(1<<MUX1)|(1<<MUX2)|(1<<MUX3); 
-    // Setando referencia interna 1.1v
-    ADMUX |= (1<<REFS1);
-    // HAbilita conversão
-    ADCSRA |= (1<<ADEN);
-    // Aguarda estabilização da tensão de referência
-    delay(20);
-    // Inicia conversão
-    ADCSRA |= (1<<ADSC);
-    // Aguarda fim da conversão
-    while (test_Bit(ADCSRA, ADSC));
-    
-    temp_internal = (1.22*((ADCH << 8) | ADCL)) + of_set;
-    
-    return temp_internal;
-}
