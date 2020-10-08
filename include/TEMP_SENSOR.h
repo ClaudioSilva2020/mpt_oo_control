de625ff64b207dc52ffa1173ae9aacb84b87fca2
claudiosilva <claudioccdsilva@gmail.com>
2020-10-08 12:43:21 -0300

    implementado LCD I2C

--- include/TEMP_SENSOR.h
+++ include/TEMP_SENSOR.h
@@ -26,22 +26,29 @@ unsigned int TEMP_read();
 
 unsigned int TEMP_read()
 {
-    float of_set = 300;
-    double temp_internal;
+    
+    
+    unsigned int temp_internal;
     // Seta modo de leitura de temperatura interna
     ADMUX |= (1<<MUX0)|(1<<MUX1)|(1<<MUX2)|(1<<MUX3); 
     // Setando referencia interna 1.1v
     ADMUX |= (1<<REFS1);
     // HAbilita conversão
-    ADCSRA |= (1<<ADEN);
+    ADCSRA = (1<<ADPS2) | (1<<ADPS1) | (1<<ADEN);
+    
+    // ADCSRA |= (1<<ADEN);
     // Aguarda estabilização da tensão de referência
-    delay(20);
+    
+    // delay(20);
+    
+    DIDR0 |= (1<<ADC2D);
     // Inicia conversão
     ADCSRA |= (1<<ADSC);
+    
     // Aguarda fim da conversão
     while (test_Bit(ADCSRA, ADSC));
     
-    temp_internal = (1.22*((ADCH << 8) | ADCL)) + of_set;
+    temp_internal = ((ADCH << 8) | ADCL);
     
     return temp_internal;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -16,19 +16,25 @@
 #include "EEPROM_DATA.h"
 #include "TEMP_SENSOR.h"
 #include "PWM.h"
+#include "LiquidCrystal85_I2C.h"
+#include "TinyWireM.h"
+// #include "Wire.h"
 
 
 #define COMAND_CH PB1
 #define STATE_RELAY PB2
 #define ADDRESS_TON 0x00
 #define ADDRESS_TOFF 0x01
+#define addressLCD 0x20
+#define COLS 16
+#define ROWS 2
 
 
 
 // Protótipo de finção
 void init_module(uint8_t timer_on, uint8_t timer_off);
 void my_delay_ms(unsigned ms);
-
+void _init_monitor(void);
 // timer_on tempo em nível lógico alto
 // timer_off tempo em nível lógico baixo
 typedef struct 
@@ -39,20 +45,23 @@ typedef struct
 
 // instância do timer_temp
 timers_temp timers_temp_t; 
+unsigned int TEMPERATURE_read;
 
+LiquidCrystal_I2C lcd(addressLCD, COLS, ROWS);
 
-int main()
+void setup()
 {
+  _init_monitor();
   int value_on, value_off;
-  float TEMPERATURE_read;
+  // unsigned int TEMPERATURE_read;
   // Bit 0 e 1 como saída e bit 2 e 3 como entrada
   // Pull-Up habilitado e as saída em nível lógico baixo
-  DDRB |= 0x03; 
-  PORTB |= 0x0C;
+  // DDRB |= 0x03; 
+  // PORTB |= 0x0C;
+  
   value_on = EEPROM_read(ADDRESS_TON);
   value_off = EEPROM_read(ADDRESS_TOFF);
 
-  Serial.begin(9600);
 
   if ((value_on) == 0xFF)
   {
@@ -64,27 +73,24 @@ int main()
   }
   
   
-  init_module(EEPROM_read(ADDRESS_TON), EEPROM_read(ADDRESS_TOFF));
-  PWM_alter_rate(128);
-  
-
-
-
-  while (1)
-  {
-    TEMPERATURE_read = TEMP_read();
-    Serial.println(TEMPERATURE_read);
-
-    if (TEMPERATURE_read >= 70)
-    {
-      PWM_alter_rate(255);
-      EEPROM_write(0x02, TEMPERATURE_read);
-    }else
-    {
-      PWM_alter_rate(128);
-    }
-  }
+  // init_module(EEPROM_read(ADDRESS_TON), EEPROM_read(ADDRESS_TOFF));
+  // PWM_alter_rate(128);
 }
+  void loop()
+  {
+    // _init_monitor();
+    /*TEMPERATURE_read = (TEMP_read() - 235)/1.22;
+    if (TEMPERATURE_read >= 33)
+    {
+      PWM_alter_rate(230);
+      EEPROM_write(0x02, TEMPERATURE_read);
+      set_Bit(PORTB, PB1);
+    }else if (TEMPERATURE_read < 28)
+    {
+      PWM_alter_rate(100);
+    }*/
+  }
+
 
 // ----------------------------------Função de inicialização----------------------------------//
 void init_module(uint8_t timer_on, uint8_t timer_off)
@@ -111,4 +117,27 @@ void my_delay_ms(unsigned ms)
     _delay_ms(1);
     --ms;
   }
+}
+
+void _init_monitor(void)
+{
+  //Inicialização do Display
+  lcd.backlight(); 
+  lcd.init(); 
+  lcd.clear(); 
+
+  //Mostrando início da maquina
+  lcd.setCursor(0, 0);
+  lcd.print("...Iniciando...");
+  lcd.setCursor(0, 1);
+  lcd.print("Controlador MPT");
+  my_delay_ms(2000);
+  lcd.clear();
+
+  // Aguardando comandos
+  lcd.setCursor(0, 0);
+  lcd.print("Controlador MPT");
+  lcd.setCursor(0, 1);
+  lcd.print(" Aguardando CMD ");
+  my_delay_ms(20);
 }
